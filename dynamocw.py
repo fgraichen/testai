6d99252ffb27f011d70ba8815127010a1b515a95
Fred Graichen <fredgraichen@yahoo.com>
2023-11-22 15:36:47 -0600

    updated 1122

--- dynamocw.py
+++
@@ -1,1 +0,0 @@
-#create python funtion to update  dynamodb table named pettracker with a new entry with model_id = "claude" and DT = timestamp and  attribute prompt = prompt and attribute result = result
